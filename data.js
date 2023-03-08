ede4e33c1e7e4b3e1945a3f51fd59c9ddebbe066
kouski <72550619+kouski@users.noreply.github.com>
2023-03-08 19:06:50 +0100

    simplificar main.js e importar datos

---
+++ data.js
@@ -0,0 +1,16 @@
+ export const CharacterData = {
+    hero: {
+      name: "Wizard",
+      avatar: "imagenes/istockphoto-1329876487-170667a.jpg",
+      health: 60,
+      diceCount: 3,
+    },
+  
+    monster: {
+      name: "Orc",
+      avatar:
+        "imagenes/depositphotos_542324844-stock-photo-portrait-savage-orc-brute-wearing.jpg",
+      health: 10,
+      diceCount: 1,
+    },
+  };
--- main.js
+++ main.js
@@ -1,50 +1,15 @@
-const hero = {
-  elementId: "hero",
-  name: "Wizard",
-  avatar: "imagenes/istockphoto-1329876487-170667a.jpg",
-  health: 60,
-  diceCount: 3
-};
+import { CharacterData } from "./data.js";
+import { Character } from "./Character.js";
 
-const monster = {
-  elementId: "monster",
-  name: "Orc",
-  avatar:
-    "imagenes/depositphotos_542324844-stock-photo-portrait-savage-orc-brute-wearing.jpg",
-  health: 10,
-  diceCount: 1
-};
 
-function getDiceRollArray(diceCount){
-  
-  return new Array(diceCount).fill(0).map(()=>{
-    return Math.floor(Math.random() * 6)+ 1;
-  })
+const wizard = new Character(CharacterData.hero);
+const orc = new Character(CharacterData.monster);
+
+function render() {
+  document.getElementById('hero').innerHTML =
+    wizard.getCharacterHtml();
+  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
-
-function getDiceHtml(diceCount){
-  return getDiceRollArray(diceCount).map(num=>{
-    return `<div class="dice">${num}</div>`
-  }).join('')
-  };
-  
+render();
 
 
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data;
-  
-  const diceHtml = getDiceHtml(diceCount)
-
-  document.getElementById(elementId).innerHTML = `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health}</b></p>
-           
-        ${diceHtml}
-            
-        
-    </div>`;
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
